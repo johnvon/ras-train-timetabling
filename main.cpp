711cdf1471619fa626588e5fa908d19e8bd53741
Alberto Santini <santini.alberto@gmail.com>
2014-05-01 18:42:24 +0200

    Added graph creation to temporary main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
+#ifndef MAIN_CPP
+#define MAIN_CPP
+
+#include <vector>
+using std::vector;
+#include <memory>
+
+#include <network/graph.h>
+#include <network/train.h>
 #include <preprocessing/data.h>
+#include <preprocessing/graph_generator.h>
 
 int main() {
     Data data("data/toy.json");
     data.print();
+    
+    vector<std::shared_ptr<Graph>> graphs;
+    for(const std::shared_ptr<Train> t : data.trains) {
+        graphs.push_back(GraphGenerator::create_graph(data, t));
+    }
+    
     return 0; 
 }
+
+#endif
