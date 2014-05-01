fd2ac47d0d10698790ee181e1dd0a398d50e6837
Alberto Santini <santini.alberto@gmail.com>
2014-05-01 18:43:35 +0200

    Added id to Node

--- network/node.h
+++ network/node.h
@@ -7,10 +7,11 @@
 
 class Node {
 public:
+    int                         id;
     std::shared_ptr<Junction>   junction;
     int                         time_interval;
     
-    Node(std::shared_ptr<Junction> junction, const int time_interval) : junction(junction), time_interval(time_interval) {}
+    Node(const int id, std::shared_ptr<Junction> junction, const int time_interval) : id(id), junction(junction), time_interval(time_interval) {}
 };
 
 #endif
