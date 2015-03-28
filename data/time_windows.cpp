a61cf632a82409dfc7e9451c59aa842e604f86e9
Alberto Santini <santini.alberto@gmail.com>
2015-03-28 16:19:32 +0100

    Adjusted time windows for weird ras input data

--- data/time_windows.cpp
+++ data/time_windows.cpp
@@ -6,11 +6,7 @@ using namespace boost::property_tree;
 using namespace boost;
 
 time_windows::time_windows(const ptree& pt, unsigned int ni) {
-    wt_left = pt.get<unsigned int>("want_time_tw_start");
-    wt_right = pt.get<unsigned int>("want_time_tw_end");
-    sa_right = pt.get<unsigned int>("schedule_tw_end");
-    
-    assert(wt_left < ni);
-    assert(wt_right < ni);
-    assert(sa_right < ni);
+    wt_left = std::min(ni, pt.get<unsigned int>("want_time_tw_start"));
+    wt_right = std::min(ni, pt.get<unsigned int>("want_time_tw_end"));
+    sa_right = std::min(ni, pt.get<unsigned int>("schedule_tw_end"));
 }
