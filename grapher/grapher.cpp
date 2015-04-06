f80e5c6062fdb9b1611bc9a4881531b8c70036e9
Alberto Santini <santini.alberto@gmail.com>
2015-04-06 21:07:36 +0200

    Gnuplot grapher

--- grapher/grapher.cpp
+++ grapher/grapher.cpp
@@ -1,5 +1,99 @@
 #include <grapher/grapher.h>
+#include <grapher/gnuplot-iostream.h>
 
-auto grapher::write_data() -> void {
+auto grapher::generate_points() -> grapher::points_data {
+    auto points = points_data();
     
+    for(auto i = 0u; i < d.nt; i++) {
+        auto current_seg = 0u;
+        auto current_time = 0u;
+        auto current_entry_time = -1l;
+        points.at(i) = series_data();
+    
+        while(current_seg != d.ns + 1) {
+            auto next_seg = -1l;
+            auto escaping = false;
+            
+            for(auto s : d.gr.delta[i][current_seg]) {
+                if(x.at(i).at(current_seg).at(current_time).at(s) > 0u) {
+                    next_seg = static_cast<long>(s);
+                }
+            }
+            
+            if(next_seg < 0 && current_time == d.ni) {
+                if(x.at(i).at(current_seg).at(d.ni).at(d.ns + 1) > 0u) {
+                    next_seg = static_cast<long>(d.ns + 1);
+                    escaping = true;
+                }
+            }
+                
+            if(next_seg >= 0) {
+                if(next_seg != current_seg) {
+                    if(current_entry_time >= 0l) {
+                        // Coming from another segment
+                        
+                        if(next_seg == static_cast<long>(d.ns + 1)) {
+                            // If arriving in tau
+                            
+                            if(escaping) {
+                                // If escaping
+                                
+                                auto distance = (
+                                    d.trn.is_eastbound.at(i) ?
+                                    d.seg.w_min_dist.at(current_seg) + d.seg.length.at(current_seg) :
+                                    d.seg.w_min_dist.at(current_seg)
+                                );
+                                points.at(i).push_back(std::make_pair(current_time, distance));
+                            } else {
+                                // If concluding the journey
+                                
+                                auto distance = (
+                                    d.trn.is_eastbound.at(i) ?
+                                    d.seg.w_min_dist.at(current_seg) + d.seg.length.at(current_seg) : 
+                                    0
+                                );
+                                points.at(i).push_back(std::make_pair(current_time, distance));
+                            }
+                        } else {
+                            // If arriving at a normal segment
+                            
+                            auto distance = (
+                                d.trn.is_eastbound.at(i) ?
+                                d.seg.w_min_dist.at(next_seg) :
+                                d.seg.w_min_dist.at(current_seg)
+                            );
+                            points.at(i).push_back(std::make_pair(current_time, distance));
+                        }
+                    } else {
+                        // If just starting its journey
+                        
+                        auto distance = (
+                            d.trn.is_eastbound.at(i) ?
+                            0 :
+                            d.seg.w_min_dist.at(next_seg) + d.seg.length.at(next_seg)
+                        );
+                        points.at(i).push_back(std::make_pair(current_time, distance));
+                    }
+                    
+                    current_entry_time = static_cast<long>(current_time + 1);
+                }
+                current_time++;
+                current_seg = next_seg;
+            }
+        }
+    }
+    
+    return points;
+}
+
+auto grapher::write_graph() -> void {
+    auto points = generate_points();
+    Gnuplot gp;
+    
+    for(auto i = 0u; i < d.nt; i++) {
+        gp << "set terminal png size 2048" << std::endl;
+        gp << "set output \"graph.png\"" << std::endl;
+        gp << "plot '-' title \"Trains meet-pass plan\" with lines" << std::endl;
+        gp.send1d(points.at(i));
+    }
 }
--- grapher/grapher.h
+++ grapher/grapher.h
@@ -4,6 +4,9 @@
 #include <data/array.h>
 #include <data/data.h>
 
+#include <utility>
+#include <vector>
+
 /*! \brief This class takes care of outputting the results in a nice format, in order to create graphs */
 struct grapher {
     /*! The problem data */
@@ -16,7 +19,13 @@ struct grapher {
     grapher(const data& d, const uint_matrix_4d& x) : d{d}, x{x} {}
     
     /*! Write data to file */
-    auto write_data() -> void;
+    auto write_graph() -> void;
+    
+private:
+    using series_data = std::vector<std::pair<double, double>>;
+    using points_data = std::vector<series_data>;
+    
+    auto generate_points() -> points_data;
 };
 
 #endif
