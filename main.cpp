6696da385b528c686f390dc1f7117bd40dc8c38b
Alberto Santini <santini.alberto@gmail.com>
2015-05-04 20:52:45 +0200

    Default for main is now sequential_solver
    This will be replaced with proper command line options parsing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <data/data.h>
 #include <params/params.h>
 #include <solver/solver.h>
+#include <solver/sequential_solver.h>
+
+#include <iostream>
 
 int main(int argc, char* argv[]) {
     auto p = params(argv[2]);
     auto d = data(argv[1], p);
-    auto s = solver(d);
+    // auto s = solver(d);
+    auto s = sequential_solver(d);
 
-    s.solve();
+    // s.solve();
+    s.solve_sequentially();
 
     return 0;
 }
