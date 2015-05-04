6daf1f0bfc3034fa16fea32266776f1b985bdc0f
Alberto Santini <santini.alberto@gmail.com>
2015-05-04 20:51:40 +0200

    Revamped the path to have dummy and empty special cases

--- data/path.cpp
+++ data/path.cpp
@@ -2,79 +2,124 @@
 
 #include <iostream>
 
-path::path(const data& d, unsigned int train, uint_matrix_3d x) : d{d}, train{train}, x{x} {
-    auto current_seg = 0u;
-    auto current_time = 0u;
+path::path(const data& d, unsigned int train, uint_matrix_3d x, double cost) : d{&d}, train{train}, x{x}, cost{cost} {
+    if(x.at(0).at(0).at(d.ns + 1) > 0u) {
+        // Dummy path!
+        make_dummy();
+    } else {
+        // Normal path!
+        auto current_seg = 0u;
+        auto current_time = 0u;
     
-    while(current_seg != d.ns + 1) {
-        auto next_seg = -1;
+        while(current_seg != d.ns + 1) {
+            auto next_seg = -1;
         
-        for(auto s : d.gr.delta.at(train).at(current_seg)) {
-            if(x.at(current_seg).at(current_time).at(s) > 0u) {
-                next_seg = static_cast<long>(s);
+            for(auto s : d.gr.delta.at(train).at(current_seg)) {
+                if(x.at(current_seg).at(current_time).at(s) > 0u) {
+                    next_seg = static_cast<long>(s);
+                    break;
+                }
+            }
+        
+            if(next_seg < 0 && current_time == d.ni) {
+                if(x.at(current_seg).at(d.ni).at(d.ns + 1) > 0u) {
+                    next_seg = static_cast<long>(d.ns + 1);
+                }
+            }
+        
+            if(next_seg >= 0) {
+                if(p.size() == 0) {
+                    p.push_back({0, current_time});
+                }
+                p.push_back({static_cast<unsigned int>(next_seg), current_time + 1});
+                current_seg = next_seg;
+            }
+        
+            if(current_time >= d.ni + 1) {
+                std::cerr << "Reached end of time horizon without being at the sink node!" << std::endl;
+                std::cerr << "Path: ";
+                for(const auto& n : p) {
+                    std::cerr << "(" << n.seg << ", " << n.t << ") ";
+                }
+                std::cerr << std::endl;
                 break;
             }
+        
+            current_time++;
         }
-        
-        if(next_seg < 0 && current_time == d.ni) {
-            if(x.at(current_seg).at(d.ni).at(d.ns + 1) > 0u) {
-                next_seg = static_cast<long>(d.ns + 1);
-            }
-        }
-        
-        if(next_seg >= 0) {
-            if(p.size() == 0) {
-                p.push_back({0, current_time});
-            }
-            p.push_back({static_cast<unsigned int>(next_seg), current_time + 1});
-            current_seg = next_seg;
-        }
-        
-        assert(current_time <= d.ni + 1);
-        
-        current_time++;
     }
 }
 
-auto path::print_summary() const -> void {
-    std::cout << "** Train: " << train << " **" << std::endl;
+path::path(const data& d, unsigned int train) : d{&d}, train{train}, cost{0.0} {
+    make_dummy();
+}
+
+auto path::make_dummy() -> void {
+    x = uint_matrix_3d(d->ns + 2, uint_matrix_2d(d->ni + 2, uint_vector(d->ns + 2, 0u)));
+    x.at(0).at(0).at(d->ns + 1) = 1u;
+    p = bv<node>();
+    p.push_back(node(0u, 0u));
+    p.push_back(node(d->ns + 1, 1u));
+    cost = 0.0;
+}
+
+auto path::make_empty() -> void {
+    x = uint_matrix_3d(d->ns + 2, uint_matrix_2d(d->ni + 2, uint_vector(d->ns + 2, 0u)));
+    p = bv<node>();
+    cost = 0.0;
+}
+
+auto path::is_dummy() const -> bool {
+    if(x.at(0).at(0).at(d->ns + 1) > 0u) {
+        assert(p.size() == 2u);
+    }
+    
+    return (x.at(0).at(0).at(d->ns + 1) > 0u);
+}
+
+auto path::is_empty() const -> bool {
+    return (p.size() == 0);
+}
+
+auto path::print_summary(std::ostream& where) const -> void {
+    where << "** Train: " << train << " **" << std::endl;
     
     auto current_seg = 0u;
     auto current_time = 0u;
     auto current_entry_time = -1;
 
-    while(current_seg != d.ns + 1) {
+    while(current_seg != d->ns + 1) {
         auto next_seg = -1;
                     
-        if(current_time >= d.ni + 1) {
+        if(current_time >= d->ni + 1) {
             std::cerr << "Could not find a valid path!" << std::endl;
             break;
         }
         
-        for(auto s : d.gr.delta.at(train).at(current_seg)) {
+        for(auto s : d->gr.delta.at(train).at(current_seg)) {
             if(x.at(current_seg).at(current_time).at(s) > 0u) {
                 next_seg = static_cast<long>(s);
                 break;
             }
         }
         
-        if(next_seg < 0 && current_time == d.ni) {
-            if(x.at(current_seg).at(d.ni).at(d.ns + 1) > 0u) {
-                next_seg = static_cast<long>(d.ns + 1);
+        if(next_seg < 0 && current_time == d->ni) {
+            if(x.at(current_seg).at(d->ni).at(d->ns + 1) > 0u) {
+                next_seg = static_cast<long>(d->ns + 1);
             }
         }
             
         if(next_seg >= 0) {
             if(static_cast<unsigned int>(next_seg) != current_seg) {
                 if(current_entry_time >= 0l) {
-                    std::cout << "\tLeaving at time: " << current_time << std::endl;
-                    std::cout << "\tRunning time: " << (current_time - current_entry_time + 1) << std::endl;
-                    std::cout << "\tMinimum running time: " << d.net.min_travel_time.at(train).at(current_seg) << std::endl;
+                    where << "\tLeaving at time: " << current_time << std::endl;
+                    where << "\tRunning time: " << (current_time - current_entry_time + 1) << std::endl;
+                    where << "\tMinimum running time: " << d->net.min_travel_time.at(train).at(current_seg) << std::endl;
                 }
                 
-                if(next_seg != static_cast<long>(d.ns + 1)) {
-                    std::cout << "Segment " << next_seg << std::endl;
-                    std::cout << "\tEntering at time: " << current_time + 1 << std::endl;
+                if(next_seg != static_cast<long>(d->ns + 1)) {
+                    where << "Segment " << next_seg << std::endl;
+                    where << "\tEntering at time: " << current_time + 1 << std::endl;
                 }
                 
                 current_entry_time = static_cast<long>(current_time + 1);
--- data/path.h
+++ data/path.h
@@ -4,6 +4,8 @@
 #include <data/array.h>
 #include <data/data.h>
 
+#include <ostream>
+
 /*! \brief This class describes the path of a train in the network */
 struct path {
     /*! This class simply repreesnts a segment at a certain time interval */
@@ -18,8 +20,8 @@ struct path {
         node(unsigned int seg, unsigned int t) : seg{seg}, t{t} {}
     };
     
-    /*! The problem data */
-    const data& d;
+    /*! A pointer to the problem data */
+    const data* d;
     
     /*! Id of the train */
     unsigned int train;
@@ -30,11 +32,29 @@ struct path {
     /*! The succession of nodes visited by the train */
     bv<node> p;
     
+    /*! The cost of the path */
+    double cost;
+    
     /*! Constructs the path starting from the variables matrix */
-    path(const data& d, unsigned int train, uint_matrix_3d x);
+    path(const data& d, unsigned int train, uint_matrix_3d x, double cost);
+    
+    /*! Makes a dummy path for the train - It goes from sigma to tau and costs nothing */
+    path(const data& d, unsigned int train);
+    
+    /*! Marks the path as dummy */
+    auto make_dummy() -> void;
+    
+    /*! Marks the path as empty, i.e. the path of a train yet to schedule - this is different fro ma dummy path */
+    auto make_empty() -> void;
     
     /*! Prints a human-readable summary of the path */
-    auto print_summary() const -> void;
+    auto print_summary(std::ostream& where) const -> void;
+    
+    /*! Tells wether the path is dummy or not */
+    auto is_dummy() const -> bool;
+    
+    /*! Tells wether the path is empty or not */
+    auto is_empty() const -> bool;
 };
 
 #endif
