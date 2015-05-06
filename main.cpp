2acc3ee98c4bac3035a920af893956b22d7aa1e1
Alberto Santini <santini.alberto@gmail.com>
2015-05-06 15:50:51 +0200

    Added ifdefs

--- main.cpp
+++ main.cpp
@@ -1,18 +1,21 @@
 #include <data/data.h>
 #include <params/params.h>
-#include <solver/solver.h>
-#include <solver/sequential_solver.h>
+
+#ifdef USE_CPLEX
+    #include <solver/solver.h>
+    #include <solver/sequential_solver.h>
+#endif
 
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     auto p = params(argv[2]);
     auto d = data(argv[1], p);
-    // auto s = solver(d);
-    auto s = sequential_solver(d);
 
-    // s.solve();
-    s.solve_sequentially();
+    #ifdef USE_CPLEX
+        auto s = sequential_solver(d);
+        s.solve_sequentially();
+    #endif
 
     return 0;
 }
