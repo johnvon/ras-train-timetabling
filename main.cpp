7016138349c802f7e4736ad8e5bdbf29ab369bf2
Alberto Santini <santini.alberto@gmail.com>
2015-05-06 15:54:38 +0200

    Moved ifdef to if

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <data/data.h>
 #include <params/params.h>
 
-#ifdef USE_CPLEX
+#if USE_CPLEX
     #include <solver/solver.h>
     #include <solver/sequential_solver.h>
 #endif
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
     auto p = params(argv[2]);
     auto d = data(argv[1], p);
 
-    #ifdef USE_CPLEX
+    #if USE_CPLEX
         auto s = sequential_solver(d);
         s.solve_sequentially();
     #endif
