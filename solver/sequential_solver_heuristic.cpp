0e19ada5396d9fcabc82e9d7ada6a76249b0ffc8
slaierno <simonelaierno@gmail.com>
2015-05-07 15:36:36 +0200

    Added class for heuristic solver

---
+++ solver/sequential_solver_heuristic.cpp
@@ -0,0 +1,12 @@
+/*
+ * sequential_solver_heuristic.cpp
+ *
+ *  Created on: 07/mag/2015
+ *      Author: simone
+ */
+
+#include "sequential_solver_heuristic.h"
+
+auto solve_sequentially() -> boost::optional<bv<path>> {
+
+}
---
+++ solver/sequential_solver_heuristic.h
@@ -0,0 +1,19 @@
+/*
+ * sequential_solver_heuristic.h
+ *
+ *  Created on: 07/mag/2015
+ *      Author: simone
+ */
+
+#ifndef SOLVER_SEQUENTIAL_SOLVER_HEURISTIC_H_
+#define SOLVER_SEQUENTIAL_SOLVER_HEURISTIC_H_
+
+#include "sequential_solver.h"
+
+struct sequential_solver_heuristic: public sequential_solver {
+public:
+	/*! Schedule the trains one by one heuristically */
+	    auto solve_sequentially() -> boost::optional<bv<path>>;
+};
+
+#endif /* SOLVER_SEQUENTIAL_SOLVER_HEURISTIC_H_ */
