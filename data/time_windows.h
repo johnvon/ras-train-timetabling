c7079d6e2895e94353c4980078fe7a3eb7d6b059
slaierno <simonelaierno@gmail.com>
2015-05-08 17:52:09 +0200

    Refuses

--- data/time_windows.h
+++ data/time_windows.h
@@ -5,10 +5,10 @@
 
 /*! This class contains info on the time windows at the arrival terminal and at SA points */
 struct time_windows {
-    /*! Dimension of the left half-tw, starting from the train's want time at its arrival terimnal */
+    /*! Dimension of the left half-tw, starting from the train's want time at its arrival terminal */
     unsigned int wt_left;
     
-    /*! Dimension of the right half-tw, starting from the train's want time at its arrival terimnal */
+    /*! Dimension of the right half-tw, starting from the train's want time at its arrival terminal */
     unsigned int wt_right;
     
     /*! Dimension of the right (and only) half-tw, starting from the train's SA time at its SA points */
