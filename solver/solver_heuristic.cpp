4884a511f2f3603ad9ed956c5f7aa73fb39c93fb
slaierno <simonelaierno@gmail.com>
2015-05-11 17:04:38 +0200

    Implementing dijkstra

--- solver/solver_heuristic.cpp
+++ solver/solver_heuristic.cpp
@@ -9,14 +9,30 @@
 
 auto solve(){
 
-	path p = path(d, train);
+	//path p = path(d, train);
 	graph * gr = &d.gr;
 	trains * trn = &d.trn;
 	auto sigma = trn->orig_ext[train];
 	auto tau = trn->dest_ext[train];
 
+	return dijkstra_time_expanded(gr, trn, src, dst);
 
 }
 
+auto dijkstra_time_expanded(graph * gr, trains * trn, auto start, auto src, auto dst) {
+	unsigned int inf = 0xFFFFFFFF - 1;
+	uint_vector dist = uint_vector(gr->n_nodes[train], inf);
+	uint_vector previous = uint_vect(gr->n_nodes[train], scr);
 
+	dist[scr] = 0;
+
+	for (const auto& next : gr->delta[train][src]) {
+		dist[next] = gr->costs[train][src][start][next];
+	}
+
+	std::set<unsigned int> S;
+	while (true) {
+
+	}
+}
 
