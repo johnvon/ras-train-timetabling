4713e5d818f05a1d0ec3acb55a26b3fe40d2e585
Giacomo Dall'Olio <giacomo.dallolio2@studio.unibo.it>
2015-05-12 17:42:54 +0200

    Minor changes

--- solver/solver_heuristic.cpp
+++ solver/solver_heuristic.cpp
@@ -11,7 +11,6 @@ using uint_pair = std::pair<unsigned int, unsigned int>;
 
 auto solve(){
 
-	//path p = path(d, train);
 	graph * gr = &d.gr;
 	trains * trn = &d.trn;
 	auto sigma = trn->orig_ext[train];
@@ -23,24 +22,33 @@ auto solve(){
 
 auto dijkstra_time_expanded(graph * gr, trains * trn, auto start, auto src_segment, auto dst_segment) {
 	unsigned int inf = 0xFFFFFFFF - 1;
-	bv<bv<uint_pair>> previous = bv(gr->v[train].size(), bv(gr->v[train][start].size(), std::make_pair(src_segment, start)));
+	bv<bv<uint_pair>> previous = bv(gr->v[train].size(), bv(gr->v[train][src_segment].size(), std::make_pair(src_segment, start)));
 
 	std::set<uint_pair> S;
 	S.insert(std::make_pair(src_segment, start));
 
-	unsigned int now = start;
+	unsigned int end;
 	bool dst_found = false;
+
 	while (!dst_found) {
 		for(const uint_pair& explored : S){
 			for (const unsigned int& connected : gr->delta[train][explored.first]){
-				if(gr->adj[train][explored.first][explored.second][connected] && S.find(std::make_pair(connected, explored.second+1)!=S.end())
-				if(S.find(connected)==S.end()){
-					S.insert(connected);
-					previous[connected]=explored;
+				if(gr->adj[train][explored.first][explored.second][connected] && S.find(std::make_pair(connected, explored.second+1))!=S.end()){
+					previous[connected][explored.second+1]=explored;
+					S.insert(std::make_pair(connected, explored.second+1));
+					if(connected==dst_segment){
+						end=explored.second+1;
+						dst_found=true;
+						break;
+					}
 				}
 			}
+			if(dst_found)
+				break;
 		}
 
 	}
+	path p =
+
 }
 
