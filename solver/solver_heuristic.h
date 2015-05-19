0f6cbc9466fa040ec9c97a54a53524f74b5c5fd2
Giacomo Dall'Olio <giacomo.dallolio2@studio.unibo.it>
2015-05-19 19:00:31 +0200

    Added comments

--- solver/solver_heuristic.h
+++ solver/solver_heuristic.h
@@ -28,9 +28,19 @@ public:
 
 private:
 	auto dijkstra_extra_greedy(graph * gr, trains * trn, auto start, auto src_segments, auto dst_segments) -> bv<node>;
+
+	/* Returns the sequence of nodes (segment, instant) of the schedule for "train" with free network */
 	auto simple_single_scheduler() -> bv<node>;
+
+	/* Returns the time to wait in case the segment is under maintenance (0 if free) with free network*/
 	auto mow_wait_time(unsigned int seg, unsigned int now) -> unsigned int;
+
+	/* Insert the nodes relative to the wait on current segment (in case the next one is under maintenance) and to reach
+	 * the end of the next segment with free network
+	 */
 	auto wait_and_travel(unsigned int here, unsigned int next, unsigned int& now, bv<node> &seq, bool &finished)->void;
+
+	/* Returns the next segment (policy: main preferred > siding > crossover > main unpreferred) */
 	auto inline choose_next(unsigned int here, unsigned int now) -> unsigned int;
 };
 
